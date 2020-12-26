9dfdf92edc10e144b84d3cc279f69a7caa8d8b0e
Rukotana Freddy <rukotanafreddy@gmail.com>
2020-12-26 23:52:50 +0200

    Initial Commit

--- models/db/migrate.js
+++ models/db/migrate.js
@@ -1,20 +1,27 @@
-const { CREATE_TABLE_USERS} = require('../configs/queries');
+const { CREATE_TABLE_USERS, select_users } = require('../configs/queries');
 
-// const {client} = require('../configs/connect-db');
-const connectDB = require('../configs/connect-db');
+
 const client = require('../configs/connect-db');
 
 const migrateAllTables = async ( isDone) => {
-    await client.query({text:CREATE_TABLE_USERS}, (err, results) => {
-        if(err) {
-            console.log('error selecting table' + err.stack);
+    // await client.query({text:CREATE_TABLE_USERS}, (err, results) => {
+    //     if(err) {
+    //         console.log('error selecting table' + err.stack);
+    //     }else{
+    //         console.log(results.rows);
+    //     }
+    // });
+    await client.query({text: select_users}, (err, results) =>{
+        if(err){
+            console.log('err' + err.stack );
         }else{
             console.log(results.rows);
-        }
-    });
+        } 
+    })
     if(isDone) isDone();
-    console.log('table created successfully');
-    process.exit(0);
+    console.log('tables has been created successfully');
+    console.log('DB connectd...');
+    // process.exit(0);
 }
 
 try{
@@ -27,3 +34,6 @@ try{
 }catch(err){
     console.log(err);
 }
+
+
+
---
+++ models/estblBootcamp.js
@@ -0,0 +1,4 @@
+const { CREATE_TABLE_USERS} = require('./configs/queries');
+
+
+const client = require('./configs/connect-db');
